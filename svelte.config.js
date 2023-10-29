44c141c30781a320f1dbb3cc28788ec89b481c09
Jeroen Wisse <jdwisse@gmail.com>
2023-10-29 21:42:46 +0100

    github

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
+
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({runtime: 'nodejs18.x',}),
-  },
-
-  preprocess: [vitePreprocess({})],
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
