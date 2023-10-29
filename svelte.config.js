d62f6b3c2225e29893affa3c003b20b73c2416b7
Jeroen Wisse <jdwisse@gmail.com>
2023-10-29 22:34:40 +0100

    vercel v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
+import adapter from '@sveltejs/adapter-vercel';
+ 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x',
+    }),
+  },
 };
-
+ 
 export default config;
